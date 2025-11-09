5fcfd9c21192a2841818c55041d38f4d26116592
Muhtar <Muhtar.Mahmut0519@gmail.com>
2025-11-09 12:13:23 -0500

    Playwright Automation

--- tests/reviews/sep-practice.spec.js
+++ tests/reviews/sep-practice.spec.js
@@ -2,7 +2,6 @@ import { test } from "@playwright/test";
 
 test("SEP Practice @sep", async ({ page }) => {
 
-
     const ENCODED_CREDENTIALS = Buffer.from(`${process.env.SEP_USERNAME}:${process.env.SEP_PASSWORD}`).toString("base64");
 
     await page.setExtraHTTPHeaders({
@@ -12,5 +11,20 @@ test("SEP Practice @sep", async ({ page }) => {
     await page.goto(`${process.env.SEP_QA_URL}`);
 
     await page.waitForTimeout(3000);
+
+    // Start Application Step:
+
+    let firstNameInput = page.locator("//input[@formcontrolname='firstName']");
+    await firstNameInput.fill("Muhtar");
+
+    let lastNameInput = page.locator("//input[@formcontrolname='lastName']");
+    await lastNameInput.fill("Mahmut");
+
+    let emailInput = page.locator("//input[@formcontrolname='email']");
+    await emailInput.fill("muhtar.mahmut@example.com");
+
+    let phoneInput = page.locator("//input[@formcontrolname='phoneNumber']");
+    await phoneInput.fill("05555555555");
     
+    await page.waitForTimeout(3000);
 });
