c1ea58fad40a4d023452816ad2e011c0352bdd2e
Muhtar <Muhtar.Mahmut0519@gmail.com>
2025-11-09 12:26:54 -0500

    Playwright automation

--- tests/reviews/sep-practice.spec.js
+++ tests/reviews/sep-practice.spec.js
@@ -14,28 +14,41 @@ test("SEP Practice @sep", async ({ page }) => {
 
     // Start Application Step:
 
-    let firstNameInput = page.locator("//input[@formcontrolname='firstName']");
-    await firstNameInput.fill("Muhtar");
-
-    let lastNameInput = page.locator("//input[@formcontrolname='lastName']");
-    await lastNameInput.fill("Mahmut");
-
-    let emailInput = page.locator("//input[@formcontrolname='email']");
-    await emailInput.fill("muhtar.mahmut@example.com");
-
-    let phoneInput = page.locator("//input[@formcontrolname='phoneNumber']");
-    await phoneInput.fill("05555555555");
-
-    let howDidYouHearAboutUsDropDown = page.locator("//mat-label[text()='How did you hear about us?']");
-    await howDidYouHearAboutUsDropDown.click();
-
-    await page.click("//span[text()='Email']");
-
-    let nextButton1 = page.locator("//button[@type='submit']");
-    await nextButton1.click();
+    CommonUI.completeStartApplicationStep(page, "Muhtar", "Mahmut", "mahmut@example.com", "05555555555" );
 
     await page.waitForTimeout(3000);
+
+    // Payment Plan Step:
+
+    
 
 
 
 });
+
+
+class CommonUI{
+
+    static async completeStartApplicationStep(page, firstName, lastName, email, phone){
+        let firstNameInput = page.locator("//input[@formcontrolname='firstName']");
+        await firstNameInput.fill(firstName);
+
+        let lastNameInput = page.locator("//input[@formcontrolname='lastName']");
+        await lastNameInput.fill(lastName);
+
+        let emailInput = page.locator("//input[@formcontrolname='email']");
+        await emailInput.fill(email);
+
+        let phoneInput = page.locator("//input[@formcontrolname='phoneNumber']");
+        await phoneInput.fill(phone);
+
+        let howDidYouHearAboutUsDropDown = page.locator("//mat-label[text()='How did you hear about us?']");
+        await howDidYouHearAboutUsDropDown.click();
+
+        await page.click(`//span[text()='Email']`);
+
+        let nextButton1 = page.locator("//button[@type='submit']");
+        await nextButton1.click();
+    }
+
+}
