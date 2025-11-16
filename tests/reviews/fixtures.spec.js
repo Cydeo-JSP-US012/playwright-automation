b57ec6ec04bd9a7863545a61eaf3c66764eaa0ec
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2025-11-16 13:29:45 -0500

    Playwright Automation

--- tests/reviews/fixtures.spec.js
+++ tests/reviews/fixtures.spec.js
@@ -1,5 +1,5 @@
 
-import { test } from "@playwright/test";
+import { test, firefox } from "@playwright/test";
 
 test("Context Fixture Demo", async ({ context }) => {
 
@@ -62,5 +62,39 @@ test("Browser Fixture Demo", async ({ browser }) => {
     await page4.bringToFront();
     await page4.goto("https://www.facebook.com/")
     await page4.waitForTimeout(2000);
+    
+});
+
+test("Custom context and browser configurations demo", async () => {
+    
+    let browser = await firefox.launch();  // browser
+    let context = await browser.newContext(); // instance of browser
+    let page = await context.newPage(); // new tab in instance of browser
+
+    await page.goto("https://www.google.com/");
+    await page.waitForTimeout(2000);
+
+});
+
+
+test("", async ({ request }) => {
+
+    let baseURL = "https://api.example.com";
+    
+    let response = await request.get(`${baseURL}/endPoint`);
+
+    // verify that the status code is 200.
+    await expect(response.status()).toBe(200);
+
+    // verify that content-type is application/json.
+    await expect(response.headers().get('content-type')).toBe('application/json');
+
+    // verify that the response body is a valid JSON object.
+    let jsonResponse = await response.json();
+    await expect(jsonResponse).toBeTruthy();
+
+    // verify that the first-name in jason body is Josh.
+    await expect(jsonResponse.first_name).toBe('Josh');
+
     
 });
