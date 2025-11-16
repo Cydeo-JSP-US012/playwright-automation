03c5b1cc572f9a2f8acd3392871156a378622822
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2025-11-16 13:13:39 -0500

    Playwright Automation

--- tests/reviews/fixtures.spec.js
+++ tests/reviews/fixtures.spec.js
@@ -27,8 +27,40 @@ test("Context Fixture Demo", async ({ context }) => {
      await page4.goto("https://www.facebook.com/")
      await page4.waitForTimeout(2000);
 
-    
+
 });
 
 
+test("Browser Fixture Demo", async ({ browser }) => {
 
+    let context1 = await browser.newContext(); // first instance of browser
+    let context2 = await browser.newContext(); // second instance of browser
+
+
+    let page1 = await context1.newPage(); // first tab  in first instance of browser
+    let page2 = await context1.newPage(); // second tab in first instance of browser
+
+
+    let page3 = await context2.newPage(); // first tab in second instance of browser
+    let page4 = await context2.newPage(); // second tab in second instance of browser
+
+    await page4.waitForTimeout(4000);
+
+    await page1.bringToFront();
+    await page1.goto("https://www.youtube.com/")
+    await page1.waitForTimeout(2000);
+
+    await page2.bringToFront();
+    await page2.goto("https://github.com/")
+    await page2.waitForTimeout(2000);
+
+
+    await page3.bringToFront();
+    await page3.goto("https://www.linkedin.com/")
+    await page3.waitForTimeout(2000);
+
+    await page4.bringToFront();
+    await page4.goto("https://www.facebook.com/")
+    await page4.waitForTimeout(2000);
+    
+});
